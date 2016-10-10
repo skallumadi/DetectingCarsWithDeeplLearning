4093c2431cb3bda9159619614293f4de2a962216
Alex <alexlbates@gmail.com>
2016-10-10 18:17:01 -0400

    keeping tkinter ver up to date with cv2 ver
    carried over the code/functionality introduced yesterday in the main branch. most likely the code will need to be completely migrated over to tkinter before long, so work now means less work later.

--- code/Dimension.py
+++ code/Dimension.py
@@ -2,85 +2,81 @@ import os, sys
 import cv2
 import numpy as np
 import Tkinter as tk
-import Image, ImageTk   #imagetk needed to be installed manually through apt-get
+from PIL import Image, ImageTk   #imagetk needed to be installed manually through apt-get
                         #sudo apt-get install python-imaging-tk
-
-def on_mouse(event, x, y, flags, params):
-    #defined by jeswin for use on cv2
-    if event == cv.CV_EVENT_LBUTTONDOWN:
-        print 'Start Mouse Position: '+str(x)+', '+str(y)
-        sbox = [x, y]
-        boxes.append(sbox)
-    elif event == cv.CV_EVENT_LBUTTONUP:
-        print 'End Mouse Position: '+str(x)+', '+str(y)
-        ebox = [x, y]
-        boxes.append(ebox)
-
-def cv2_loop():
-    #the gui loop written by jeswin for use with cv2
-    count = 0
-    while(1):
-        count += 1
-        _,img = cap.read()
-        img = cv2.blur(img, (3,3))
-
-        cv2.namedWindow('real image')
-        cv.SetMouseCallback('real image', on_mouse, 0)
-        cv2.imshow('real image', img)
-
-        if count < 50:
-            if cv2.waitKey(33) == 27:
-                cv2.destroyAllWindows()
-                break
-        elif count >= 50:
-            if cv2.waitKey(0) == 27:
-                cv2.destroyAllWindows()
-                break
-            count = 0
 
 class App:
     def __init__(self, window, image_path):
         self.window = window
-        window.title("Display Area")
-        window.configure(background='grey')
+        self.window.title("Display Area")
+        self.window.configure(background='grey')
+
+        self.cv2_img = self.load_cv2_image(image_path, (800,600))
+        self.tk_img = self.get_imageTK_obj(self.cv2_img)
 
         #image display area
-        img = self.load_image(image_path, (800, 600))
+        self.canvas=tk.Canvas(self.window, height=600, width=800)
+        self.canvas_image = self.canvas.create_image(0,0, image=self.tk_img, anchor=tk.NW)
+        self.canvas.image = self.tk_img #if a reference is not kept of the image, it will be garbage collected out of existence.
 
-        canvas=tk.Canvas(self.window, height=600, width=800)
-        canvas_image = canvas.create_image(0,0, image=img, anchor=tk.NW)
+        self.canvas.pack(side='bottom', fill='both', expand='yes')
 
-        canvas.image = img #if a reference is not kept of the image, it will be garbage collected out of existence.
-        canvas.pack(side='bottom', fill='both', expand='yes')
+        self.window.bind('c', self.window_exit)
+        self.window.bind('<B1-Motion>', self.draw_area)
+        self.window.bind('<ButtonRelease-1>', self.create_rectangle)
 
-        self.window.bind('<Motion>', self.motion)
+        self.current_points_list = [] #definitely should be held in a pklot class or something
 
-    def motion(self, event):
-        """ Example Tk event. This will likely end up being mouse click detection
-            that will go on to be used in drawing rectangles on the loaded image.
+    def window_exit(self, event):
+        """ listen for program exit button
         """
-        x, y = event.x, event.y
-        print str(x) + " : " + str(y)
+        self.window.destroy()
 
-    def load_image(self, filepath, dimensions):
-        """ From given filepath, load and resize an image from
-            the given path string.
-            Return a TkPhoto object to be used in the Tkinter window.
+    def draw_area(self, events):
+        """ create an area to turn into a rectangle, and draw points to show
+            what is currently recorded
         """
-        #gotta give a resize tuple.
-        #this is mainly so simplify displaying and drawing lines on the display canvas
+        self.current_points_list.append((events.x, events.y))
+
+        self.canvas.create_oval(events.x-1, events.y-1, events.x+1, events.y+1, fill="yellow")
+
+    def create_rectangle(self, events):
+        """ creates a rectangle from the points stored in self.current_points_list
+            using cv2 libraries. hopefully.
+        """
+        n_array = np.array(self.current_points_list) #convert list of points to array
+        rectangle = cv2.minAreaRect(n_array) #find points and angle of rect
+        box = cv2.cv.BoxPoints(rectangle) #convert to proper coordinate points
+        box = np.int0(box) #some numpy nonsense. required to work, dunno what it does though
+
+
+        #convert array into coordinate tuple for tkinter
+        coord_list = []
+        for i in range(4): #4 groups of coords.
+            coord_list.append(box[i][0])
+            coord_list.append(box[i][1])
+        self.canvas.create_polygon(coord_list, fill='', outline='lime')
+        self.current_points_list = []
+
+
+    def load_cv2_image(self, path, dimensions):
+        """ From the given path, load and resize a jpeg
+            and return a cv2 image object
+        """
         if not dimensions:
             raise Exception("Must provide height and width as tuple")
-        #load image through open cv library. it isnt needed right now,
-        #but going forward this could be useful as an example on converting from cv2
-        loaded_image = cv2.imread(filepath, cv2.IMREAD_COLOR)
+        img = cv2.imread(path)
+        img = cv2.resize(img, dimensions, interpolation=cv2.INTER_CUBIC)
+        return img
 
+    def get_imageTK_obj(self, cv2Img):
+        """ from the given cv2 image, return a TKImage object for display
+        """
         # the color channels need to be shuffled around due to differences between cv2 and tk
-        b,g,r = cv2.split(loaded_image)
-        shuffled_image =cv2.merge((r,g,b))
+        b,g,r = cv2.split(cv2Img)
+        shuffled_image = cv2.merge((r,g,b))
 
         img = Image.fromarray(shuffled_image)
-        img = img.resize(dimensions, Image.ANTIALIAS)
 
         #photoimage objects can be used any place that tkinter expects an image
         im_tk = ImageTk.PhotoImage(image=img)
