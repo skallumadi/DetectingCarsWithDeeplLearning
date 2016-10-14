99cb37a18fd3cd4f9c5453288b506d50c32bf129
Alex <alexlbates@gmail.com>
2016-10-14 10:07:07 -0400

    Merged branch ImageAndGuiTinkering into master

---
+++ code/App.py
@@ -0,0 +1,129 @@
+import os, sys
+import cv2
+import numpy as np
+import Tkinter as tk
+from PIL import Image, ImageTk  # imagetk needed to be installed manually. pip install Pillow
+
+from ParkingLot import *
+
+
+class App:
+    def __init__(self, window, image_path, PKLot):
+        self.parkinglot = PKLot
+
+        self.window = window
+        self.window.title("Display Area")
+        self.window.configure(background='grey')
+
+        self.cv2_img = self.load_cv2_image(image_path, (800, 600))
+        self.tk_img = self.get_imageTK_obj(self.cv2_img)
+
+        # image display area
+        self.canvas = tk.Canvas(self.window, height=600, width=800)
+        self.canvas_image = self.canvas.create_image(0, 0, image=self.tk_img, anchor=tk.NW)
+        self.canvas.image = self.tk_img  # if a reference is not kept of the image, it wont stay in memory.
+
+        self.canvas.grid(row=0, column=0, sticky='N')
+
+        # Information Labels
+        self.numMonitoredSpotsLabel = tk.Label(self.window, text='Monitored')
+        self.numOccupiedSpotsLabel = tk.Label(self.window, text='Occupied')
+        self.numVacantSpotsLabel = tk.Label(self.window, text='Vacant')
+
+        self.numMonitoredSpotsLabel.grid(row=1, column=0, sticky='W')
+        self.numOccupiedSpotsLabel.grid(row=2, column=0, sticky='W')
+        self.numVacantSpotsLabel.grid(row=3, column=0, sticky='W')
+
+        # exit button
+        self.exitButton = tk.Button(self.window, text='Quit', command=self.window.destroy)
+
+        self.exitButton.grid(row=4, column=0, sticky='E')
+
+        # bind window events
+        self.window.bind('c', self.window_exit)
+        self.window.bind('<B1-Motion>', self.draw_area)
+        self.window.bind('<ButtonRelease-1>', self.create_rectangle)
+
+        self.current_points_list = []  # definitely should be held in a pklot class or something
+
+    def window_exit(self, event):
+        """ listen for program exit button
+        """
+        self.window.destroy()
+
+    def draw_area(self, events):
+        """ create an area to turn into a rectangle, and draw points to show
+            what is currently recorded
+        """
+        self.current_points_list.append((events.x, events.y))
+
+        self.canvas.create_oval(events.x - 1, events.y - 1, events.x + 1, events.y + 1, fill="yellow", tags='indicator')
+
+    def create_rectangle(self, events):
+        """ creates a rectangle from the points stored in self.current_points_list
+            using cv2 libraries. hopefully.
+        """
+        n_array = np.array(self.current_points_list)  # convert list of points to array
+
+        try:  # someone clicked and released immediately. just ignore it pretty much.
+            rectangle = cv2.minAreaRect(n_array)  # find points and angle of rect
+            box = cv2.cv.BoxPoints(rectangle)  # convert to proper coordinate points
+            box = np.int0(box)  # some numpy nonsense. required to work, dunno what it does though
+
+            # convert array tuple thing into coordinate list for tkinter
+            coord_list = []  # in format of [x,y, x,y, x,y, x,y, x,y]
+            for i in range(4):  # 4 groups of coords.
+                coord_list.append(box[i][0])
+                coord_list.append(box[i][1])
+
+            self.parkinglot.addSpot(coord_list)
+
+        except cv2.error:
+            pass
+
+        self.current_points_list = []
+        self.canvas.delete('indicator')
+        self.draw_rectangles()
+
+    def draw_rectangles(self):
+        self.canvas.delete('parking spots')
+        for spot in parkinglot.getParkingSpots():
+            self.canvas.create_polygon(spot.location, fill='', outline='lime', tags='parking spots')
+
+    def load_cv2_image(self, path, dimensions):
+        """ From the given path, load and resize a jpeg
+            and return a cv2 image object
+        """
+        if not dimensions:
+            raise Exception("Must provide height and width as tuple")
+        img = cv2.imread(path)
+        img = cv2.resize(img, dimensions, interpolation=cv2.INTER_CUBIC)
+        return img
+
+    def get_imageTK_obj(self, cv2Img):
+        """ from the given cv2 image, return a TKImage object for display
+        """
+        # the color channels need to be shuffled around due to differences between cv2 and tk
+        b, g, r = cv2.split(cv2Img)
+        shuffled_image = cv2.merge((r, g, b))
+
+        img = Image.fromarray(shuffled_image)
+
+        # photoimage objects can be used any place that tkinter expects an image
+        im_tk = ImageTk.PhotoImage(image=img)
+        return im_tk
+
+
+if __name__ == "__main__":
+
+    image_path = "Parking-Lot.jpg"
+    try:
+        assert os.path.isfile(image_path)
+    except AssertionError:
+        print "ERROR: Invalid Image Path"
+        sys.exit()
+
+    root = tk.Tk()
+    parkinglot = ParkingLot()
+    app = App(root, image_path, parkinglot)
+    root.mainloop()
---
+++ code/ParkingLot.py
@@ -0,0 +1,90 @@
+from Spot import *
+
+
+class ParkingLot:
+    def __init__(self):
+        self.infoPath = ""  # xml file location for parking spots
+        self.numSpots = 0  # how many spots are on record
+        self.numOccupied = 0
+        self.numVacant = 0
+        self.parkingSpots = []  # a list of all the parking spots and status.
+
+        # self.currentLotImage = #the most recent image for the parking lot
+        # self.imageClassifier = #link to image classifier object to process images
+
+    def loadXML(self, filePath):
+        """ load xml file and put information in the right places.
+            number of spots, initialize parking spots list.
+        """
+
+    def getParkingSpots(self):
+        """ return a list of all parking spots in the lot
+        """
+        return self.parkingSpots
+        pass
+
+    def saveParkingSpots(self):
+        """ save the current list of parking spaces.
+            meant to be used if a parking spot is added or removed.
+        """
+        pass
+
+    def saveUsage(self, filePath):
+        """ save current lot information to separate file.
+            how many spots are open, which ones specifically, time, etc.
+        """
+        pass
+
+    def getOccupied(self):
+        """ get a list of all occupied spaces in the lot.
+        """
+        occupiedList = []
+        for spot in self.parkingSpots:
+            if spot.status == 'occupied':
+                occupiedList.append(spot)
+        return occupiedList
+        pass
+
+    def getVacant(self):
+        """ get a list of all vacant spaces in the lot.
+        """
+        vacantList = []
+        for spot in self.parkingSpots:
+            if spot.status == 'vacant':
+                vacantList.append(spot)
+        return vacantList
+        pass
+
+    def addSpot(self, coordinates):
+        """ add a region of the parking lot as a new space to be monitored.
+            coordinates is expected to be a list or tuple 8 items long.
+            4 groups of x y coordinates.
+            Ex:
+            [0, 0, 10, 14, 30, 30, 56, 67]
+             x  y  x   y   x   y   x   y
+        """
+        self.numSpots += 1
+        self.parkingSpots.append(Spot(self.numSpots, coordinates, 'vacant'))
+        # self.update(self.currentLotImage)
+        pass
+
+    def removeSpot(self, id):
+        """ remove a particular spot from the list of monitored locations.
+        """
+        for spot in self.parkingSpots:
+            if spot.idNum == id:
+                self.parkingSpots.remove(spot)
+                for i in range(len(self.parkingSpots)):  # relabel all spots to keep the id numbers
+                    self.parkingSpots[i].idNum = i  # representative of the number of spots
+                return
+
+        raise Exception("No spot with given id found.")
+        pass
+
+    def update(self, image):
+        """ update the current information for this parking lot.
+            the image given is expected to be the most recent image for the lot.
+        """
+        self.currentLotImage = image
+        self.parkingSpots = self.imageClassifier.process(self.parkingSpots)
+        pass
