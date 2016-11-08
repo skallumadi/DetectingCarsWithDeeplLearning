9f6d1edb424278322220dfa146dd11e11316187c
Alex Bates <alexlbates@gmail.com>
2016-11-07 19:43:58 -0500

    Added dialogue to select image source directory on launch
    two entry boxes on launch, the source directory and the origin directory. the source directory will be where the program gets its new images to process as normal. the origin directory is for randomly feeding images into the source directory, and the contents of it should not be modified.

--- code/App.py
+++ code/App.py
@@ -1,15 +1,17 @@
 import random
 import sys, os, shutil
+import threading
+import time
+
+import PIL.Image
+
 import Tkinter as tk
 import tkFileDialog
-import threading
-import PIL.Image
-import CustomizedInterfaceElements as ui
-from ParkingLot import *
 from PIL import ImageTk
 
-import time
-import datetime
+import CustomizedInterfaceElements as ui
+from ParkingLot import *
+
 
 class RoleSelect(tk.Frame):
     def __init__(self, parent):
@@ -29,19 +31,42 @@ class RoleSelect(tk.Frame):
         self.selectSetupButton.pack(side=tk.RIGHT)
         self.selectMonitorButton.pack(side=tk.RIGHT)
 
+        # directory paths for image source and image origin
+        self.source_label = tk.Label(self, text='Enter Image Source: ')
+        self.source_var = tk.StringVar()
+        self.source_entry = tk.Entry(self, textvariable=self.source_var)
+        self.source_label.pack(side=tk.LEFT)
+        self.source_entry.pack(side=tk.LEFT)
+
+        self.origin_label = tk.Label(self, text='Enter Image Origin: ')
+        self.origin_var = tk.StringVar()
+        self.origin_entry = tk.Entry(self, textvariable=self.origin_var)
+        self.origin_label.pack(side=tk.LEFT)
+        self.origin_entry.pack(side=tk.LEFT)
+
         self.pack()
 
     def start_setup(self):
-        self.destroy()
-        app = SetupApp(self.parent, "resources/init.jpg", ParkingLot())
+        origin = os.getcwd() + self.origin_var.get()
+        source = os.getcwd() + self.source_var.get()
+        if os.path.isdir(origin) and os.path.isdir(source):
+            self.destroy()
+            app = SetupApp(self.parent, "resources/init.jpg", ParkingLot(), source, origin)
+        else:
+            print "Invalid Directories"
 
     def start_monitor(self):
-        self.destroy()
-        app = MonitorApp(self.parent, "resources/init.jpg", ParkingLot())
+        origin = os.getcwd() + self.origin_var.get()
+        source = os.getcwd() + self.source_var.get()
+        if os.path.isdir(origin) and os.path.isdir(source):
+            self.destroy()
+            app = MonitorApp(self.parent, "resources/init.jpg", ParkingLot(), source, origin)
+        else:
+            print "Invalid Directories"
 
 
 class SetupApp(tk.Frame):
-    def __init__(self, parent, image_path, PKLot):
+    def __init__(self, parent, image_path, PKLot, img_src_dir, img_origin_dir):
         self.parent = parent
         self.parkinglot = PKLot
         self.cv2_img = cv2.imread(image_path)
@@ -52,32 +77,47 @@ class SetupApp(tk.Frame):
         self.winfo_toplevel().configure(background='grey')
 
         self.canvas = ui.CanvasArea(self, self.parkinglot, self.cv2_img)
-        self.canvas.grid(row=0, column=0, sticky=tk.N)
+        self.canvas.grid(row=0, column=0, columnspan=10, sticky=tk.N)
 
         # Parking Spot List
         # currently an issue with scrolling on the list. it will scroll and then immediately be set back to to 0.
         self.parkingspot_listbox = ui.SpotList(self, self.parkinglot, width=20, height=34)
-        self.parkingspot_listbox.grid(row=0, column=1, rowspan=3, sticky='N')
+        self.parkingspot_listbox.grid(row=0, column=10, sticky='N')
 
         # MenuBar
         self.menubar = ui.MenuBar(self)
         self.parent.configure(menu=self.menubar)
 
-        # Image Processor/Parking lot update
+        # ##Buttons
+        # Image Processor/Parking lot update button
+        tk.Grid.rowconfigure(self, 1, weight=1)
         self.lotupdate_button = tk.Button(self, text='Update Lot', command=self.update_lot)
-        self.lotupdate_button.grid(row=5, column=1, sticky='E')
-
+        self.lotupdate_button.grid(row=1, column=10, sticky='EW')
         # delete spot button
         self.deleteSpotButton = tk.Button(self, text='Delete Selected', command=self.delete_selection)
-        self.deleteSpotButton.grid(row=8, column=1, sticky='E')
-
-        #single image load Button
+        self.deleteSpotButton.grid(row=2, column=10, sticky='EW')
+        # single image load Button
         self.loadImageButton = tk.Button(self, text="load image", command=self.manual_set_image)
-        self.loadImageButton.grid(row=9, column=1, sticky='E')
-
-        #toggle update button
+        self.loadImageButton.grid(row=3, column=10, sticky='EW')
+        # toggle update button
         self.toggleUpdateButton = tk.Button(self, text="toggle update", command=self.update_toggle)
-        self.toggleUpdateButton.grid(row=10, column=1, sticky='E')
+        self.toggleUpdateButton.grid(row=4, column=10, sticky='EW')
+        # ###
+
+        # ##Labels
+        self.occupied_label = tk.Label(self, text='Occupied')
+        self.occupied_count_var = tk.StringVar()
+        self.occupied_count_label = tk.Label(self, textvariable=self.occupied_count_var)
+
+        self.occupied_label.grid(row=1, column=0, sticky='W')
+        self.occupied_count_label.grid(row=1, column=1, sticky='W')
+
+        self.vacant_label = tk.Label(self, text='Vacant')
+        self.vacant_count_var = tk.StringVar()
+        self.vacant_count_label = tk.Label(self, textvariable=self.vacant_count_var)
+
+        self.vacant_label.grid(row=2, column=0, sticky='W')
+        self.vacant_count_label.grid(row=2, column=1, sticky='W')
 
         # bind events
         self.parent.bind('c', self.window_exit)
@@ -88,13 +128,13 @@ class SetupApp(tk.Frame):
 
         self.update_toggle_bool = False
 
-        self.image_source_directory = os.getcwd() + "/resources/lot_images/"
-        self.img_origin_directory = os.getcwd() + '/resources/source_images/'
+        self.image_source_directory = img_src_dir
+        self.img_origin_directory = img_origin_dir
 
+        # image swapping thread setup
         t = threading.Thread(target=image_swapper, args=[self.img_origin_directory, self.image_source_directory])
         t.daemon = True
         t.start()
-
 
         self.update_all()  # this kicks off the main root calling updates ever 100 ms
 
@@ -112,17 +152,9 @@ class SetupApp(tk.Frame):
             pass
 
     def update_lot(self):
-        # there is absolutely no way this works as intended on first run. i refuse to believe it.
-        # there is a secret race condition or something will be corrupted or something.
-        # but it leaves the UI running while everything is going on, soooooooo.....
-
-        # was worried about stray threads piling up, but threading.enumerate seems to show that things clean up after
-        # themselves.
-
-        #self.parkinglot.update()
-
-        if threading.activeCount() > 2:
+        if threading.activeCount() > 3:
             print 'Warning: Images have not finished processing from the last iteration'
+            print 'Active Threads: ' + str(threading.activeCount())
             pass
         else:
             threading.Thread(target=self.parkinglot.update, args=[]).start()
@@ -144,13 +176,7 @@ class SetupApp(tk.Frame):
         # take the image, or the first, image from the image_source_directory.
         # currently going to try just deleting the image when done with it.
 
-        # this function relies on something else in the background supplying images.
-        # so perhaps a separate thread populating the directory with an endless cycle of images.
-        # (as a place holder for an actual camera system)
-
-        # so when function call, set the image to the image in the directory.
-        # then, update the parking lot image path
-        # then, update the canvas.
+        # this function relies on something else in the background supplying images to the folder.
 
         image = os.listdir(self.image_source_directory)
         if image:
@@ -161,38 +187,10 @@ class SetupApp(tk.Frame):
             print "Image " + image + " Removed"
             self.update_lot()
 
-
-        #images = os.listdir(self.image_source_directory)
-        #images.sort()
-        #if images:
-        #    if self.parkinglot.currentLotImage == 'resources/init.jpg':
-        #        self.parkinglot.currentLotImage = self.image_source_directory + images[0]
-        #        self.canvas.update_image()
-
-        #    else:
-        #        out_dir = 'resources/old_lot_images/' + str(datetime.datetime.fromtimestamp(time.time())).split('.')[0].replace(':', '_') + '.jpg'
-        #        os.rename(self.parkinglot.currentLotImage, out_dir)
-        #        # repetitive. the way i handle the initial image removes the new image before it gets processed.
-        #        # im tired and the only way i can think to fix it easily is to just rebuild the list here.
-        #        # definitely not a good way to handle it though.
-        #        images = os.listdir(self.image_source_directory)
-        #        if images:  # chance of the list being empty since removing files above.
-        #            images.sort()
-
-        #            self.parkinglot.currentLotImage = self.image_source_directory + images[0]
-        #            self.canvas.parkinglot.currentLotImage = self.image_source_directory + images[0]
-        #            self.canvas.update_image()
-
-        #            print 'Update Image - ' + str(datetime.datetime.fromtimestamp(time.time())).split('.')[0]
-
-        #else:
-        #    print 'No Images Found - ' + str(datetime.datetime.fromtimestamp(time.time())).split('.')[0]
-
     def update_all(self, event=None):
         if time.time() - self.timestamp > 10 and self.update_toggle_bool:
             self.timestamp = time.time()
             self.update_current_image()
-            #maybe might crash since im trying to delete images around when i call this
             self.update_lot()
 
         self.parkingspot_listbox.update_parkingspot_list()
@@ -203,14 +201,17 @@ class SetupApp(tk.Frame):
         except (IndexError, AttributeError) as e:
             pass
 
+        self.vacant_count_var.set(len(self.parkinglot.getEmpty()))
+        self.occupied_count_var.set(len(self.parkinglot.getOccupied()))
+
         self.canvas.update_all()
         self.parent.after(100, self.update_all)
 
 
 class MonitorApp(SetupApp):
     # since this is just a version of SetupApp with functionality removed, then it can just inherit directly from it ez.
-    def __init__(self, parent, imagePath, PKLot):
-        SetupApp.__init__(self, parent, imagePath, PKLot)
+    def __init__(self, parent, image_path, PKLot, img_src_dir, img_origin_dir):
+        SetupApp.__init__(self, parent, image_path, PKLot, img_src_dir, img_origin_dir)
 
         # remove the ability for users to draw things in monitor mode
         self.canvas.unbind('<B1-Motion>')
@@ -218,8 +219,7 @@ class MonitorApp(SetupApp):
 
         # remove delete button
         self.deleteSpotButton.grid_forget()
-
-        #remove save lot menuitem
+        # remove save lot menu item
         self.menubar.delete(2)
 
 
@@ -228,11 +228,10 @@ def image_swapper(source_directory, populate_directory):
     while True:
         population_images = os.listdir(populate_directory)
         if not population_images:
-            print population_images
             options = os.listdir(source_directory)
             choice = random.choice(options)
             shutil.copy(source_directory + choice, populate_directory + choice)
-        time.sleep(1)
+        time.sleep(2)
 
 
 if __name__ == "__main__":
@@ -242,6 +241,7 @@ if __name__ == "__main__":
         print "Warning: Could not locate installed Caffe, images will not be analyzed."
 
     root = tk.Tk()
+    tk.Grid.columnconfigure(root, 0, weight=1)
     root.resizable(0, 0)
 
     app = RoleSelect(root)
